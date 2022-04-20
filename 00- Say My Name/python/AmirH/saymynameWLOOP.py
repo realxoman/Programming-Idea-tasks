111cbdce668bea7c8f57cf100bf0d0c68e7f1858
AmirH <amirhabsari01@gmail.com>
2022-04-20 22:25:19 +0430

    added a loop to the program

---
+++ 00- Say My Name/python/AmirH/saymynameWLOOP.py
@@ -0,0 +1,8 @@
+print ("Now, Say my name")
+esm = ""
+while esm != "Heisenburg":
+    esm = input ()
+    if esm == "Heisenburg":
+        print ("You're goddamn right")
+    else:
+        print ("Nah")
